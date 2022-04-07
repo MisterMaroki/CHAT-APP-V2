da8b9165fc4c04f273c254fa14097c0b5bb768e0
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 12:37:00 +0100

    firebase config for one intialisation

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBSsDM58sOTUHOc6Jcshpo6ipy1FV56G-4',
+	authDomain: 'whatsapp-2-2bff4.firebaseapp.com',
+	projectId: 'whatsapp-2-2bff4',
+	storageBucket: 'whatsapp-2-2bff4.appspot.com',
+	messagingSenderId: '202800331423',
+	appId: '1:202800331423:web:c1fd91bce2e300caff00aa',
+};
+
+const app = !firebase.apps.length
+	? firebase.initialiseApp(firebaseConfig)
+	: firebase.app();
