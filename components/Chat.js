5ffa6544b2b562d624c9dcc02f6bae48a43af90e
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 17:19:10 +0100

    sidebar chat routing setup

--- components/Chat.js
+++ components/Chat.js
@@ -4,19 +4,25 @@ import getRecipientEmail from '../utils/getRecipientEmail';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
 import { useCollection } from 'react-firebase-hooks/firestore';
+import { useRouter } from 'next/router';
 
 function Chat({ id, users }) {
+	const router = useRouter();
 	console.log(id, users);
 	const [user] = useAuthState(auth);
 	const [recipientSnapshot] = useCollection(
 		db.collection('users').where('email', '==', getRecipientEmail(users, user))
 	);
 
+	const enterChat = () => {
+		router.push(`/chat/${id}`);
+	};
+
 	const recipient = recipientSnapshot?.docs?.[0]?.data();
 	const recipientEmail = getRecipientEmail(users, user);
 	console.log('Recipient email: ' + recipientEmail);
 	return (
-		<Container>
+		<Container onClick={enterChat}>
 			{recipient ? (
 				<UserAvatar src={recipient?.photoURL} />
 			) : (
