1a8806e1b9dc0b68e37e43359163a6143edfb4fd
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 12:28:59 +0100

    sidebar components and organisation - before reusdable components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, Button, IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 function Sidebar() {
 	return (
 		<Container>
@@ -16,6 +17,13 @@ function Sidebar() {
 					</IconButton>
 				</IconsContainer>
 			</Header>
+			<Search>
+				<SearchIcon />
+				<SearchInput placeholder="Search in chats" />
+			</Search>
+			<SidebarButton>Start a new chat</SidebarButton>
+
+			{/*list of chats*/}
 		</Container>
 	);
 }
@@ -24,8 +32,46 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+	display: flex;
+	position: sticky;
+	top: 0;
+	background-color: white;
+	z-index: 1;
+	justify-content: space-between;
+	align-items: center;
+	padding: 15px;
+	height: 80px;
+	border-bottom: 1px solid whitesmoke;
+`;
 
-const UserAvatar = styled(Avatar)``;
+const UserAvatar = styled(Avatar)`
+	cursor: pointer;
+
+	:hover {
+		opacity: 0.8;
+	}
+`;
+
+const Search = styled.div`
+	display: flex;
+	align-items: center;
+	padding: 20px;
+	border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+	outline: none;
+	border: none;
+	flex: 1;
+`;
 
 const IconsContainer = styled.div``;
+
+const SidebarButton = styled(Button)`
+	width: 100%;
+	&&& {
+		border-top: 1px solid whitesmoke;
+		border-bottom: 1px solid whitesmoke;
+	}
+`;
