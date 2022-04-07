0b94dce3ae870e3ca6cc3f7f666ada1e67dd6db7
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 17:16:14 +0100

    setup chat component & updata userAvatar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -43,7 +43,7 @@ function Sidebar() {
 	return (
 		<Container>
 			<Header>
-				<UserAvatar onClick={() => auth.signOut()} />
+				<UserAvatar src={user.photoURL} onClick={() => auth.signOut()} />
 				<IconsContainer>
 					<IconButton>
 						<ChatIcon />
@@ -61,7 +61,7 @@ function Sidebar() {
 
 			{/*list of chats*/}
 			{chatsSnapshot?.docs.map((chat) => (
-				<Chat key={chat.id} id={chat.id} user={chat.data().users} />
+				<Chat key={chat.id} id={chat.id} users={chat.data().users} />
 			))}
 		</Container>
 	);
