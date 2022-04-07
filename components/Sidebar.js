70c88db390fbb0c22e046a4e4712c954b402e228
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 12:01:20 +0100

    ad styled avatar and mui icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,21 @@
+import { Avatar } from '@material-ui/core';
 import styled from 'styled-components';
 function Sidebar() {
-	return <Container>Sidebar</Container>;
+	return (
+		<Container>
+			<Header>
+				<UserAvatar />
+			</Header>
+		</Container>
+	);
 }
 
 export default Sidebar;
 
 const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)`
+	margin: 10px;
+`;
