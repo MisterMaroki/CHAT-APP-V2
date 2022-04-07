e22932126ac5d5c6e803008d255a97c608b84324
Omar-macbook <omar987@hotmail.co.uk>
2022-04-07 12:05:54 +0100

    add interactive sidebar icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,16 @@
 import { Avatar } from '@material-ui/core';
 import styled from 'styled-components';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 function Sidebar() {
 	return (
 		<Container>
 			<Header>
 				<UserAvatar />
+				<IconsContainer>
+					<ChatIcon />
+					<MoreVertIcon />
+				</IconsContainer>
 			</Header>
 		</Container>
 	);
@@ -16,6 +22,6 @@ const Container = styled.div``;
 
 const Header = styled.div``;
 
-const UserAvatar = styled(Avatar)`
-	margin: 10px;
-`;
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
