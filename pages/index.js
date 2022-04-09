1a8f69a00173ba33ee2d63fb5a75609e78767019
Omar-macbook <omar987@hotmail.co.uk>
2022-04-09 18:47:46 +0100

    icon updates and more resposniveness updates

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head';
-import Image from 'next/image';
+
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Whatsapp-v2</title>
-				<link rel="icon" href="/favicon.ico" />
+				<title>Snappy-v2</title>
+				<link rel="icon" href="/logo.ico" />
 			</Head>
 			<Sidebar />
 		</div>
